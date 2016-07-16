f3ca655ab00cb1028a770eb9c46dbc84dd72371d
qeesung <1245712564@qq.com>
2016-07-16 18:25:48 +0800

    init repo

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('qeesungProfile')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
